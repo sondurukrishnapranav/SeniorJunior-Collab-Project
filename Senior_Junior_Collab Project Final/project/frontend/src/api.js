19a311e6e60b6d14872cb5b53c51ff1582d02c49
KrishnaPranav <pranav9966.sondur@gmail.com>
2026-01-27 11:52:05 +0530

    URL Fix

--- Senior_Junior_Collab Project Final/project/frontend/src/api.js
+++ Senior_Junior_Collab Project Final/project/frontend/src/api.js
@@ -1,11 +1,7 @@
-// src/api.js
 import axios from 'axios';
 
-// Automatically uses the environment variable in production, or localhost in development
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://seniorjunior-collab-project-production.up.railway.app/api",
 });
 
 export default api;
