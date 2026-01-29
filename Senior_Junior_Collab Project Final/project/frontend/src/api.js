1a5105128d2614e095a5519e5b713c4abff0a373
KrishnaPranav <pranav9966.sondur@gmail.com>
2026-01-29 18:47:00 +0530

    Commit

--- Senior_Junior_Collab Project Final/project/frontend/src/api.js
+++ Senior_Junior_Collab Project Final/project/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://seniorjunior-collab-project.onrender.com/",
+  baseURL: "https://seniorjunior-collab-project.onrender.com/api",
 });
 
 export default api;
